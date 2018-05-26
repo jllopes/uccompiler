0ad920df3b3277a256ac9d7bf989ff29f253f502
jllopes <jlglopes96@gmail.com>
2018-05-26 23:47:41 +0100

    remove unnecessary files

--- Meta4/funcdecl.c
+++
@@ -1,7 +0,0 @@
-void func_declaration_gen(Node *node){
-	char *type = types_to_llvm(node->child->token);
-	char *name = node->child->brother->value; /* Value from ID */
-	Node *aux = node->child->brother->brother->child; /* Skips until first ParamDeclaration */
-	params_gen(aux);
-	printf("")
-}
